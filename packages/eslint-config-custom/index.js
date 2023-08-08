715a7a0d325b217c2e7f1eda4ee58fcf7eeb3c82
Guillaume Pitot <guillaume@honeyinsurance.com>
2023-08-08 16:42:49 +1000

    working progress eslint

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -2,8 +2,35 @@ module.exports = {
   extends: ["turbo", "prettier"],
   rules: {},
   parserOptions: {
+    parser: "@typescript-eslint/parser",
     babelOptions: {
       presets: [],
+    },
+    extends: [
+      "eslint:recommended",
+      "plugin:@typescript-eslint/recommended",
+      "prettier",
+      "plugin:prettier/recommended",
+      "plugin:import/typescript",
+    ],
+    plugins: ["import", "@typescript-eslint"],
+    rules: {
+      "comma-dangle": 0,
+      "no-underscore-dangle": 0,
+      "no-param-reassign": 0,
+      "no-return-assign": 0,
+      camelcase: 0,
+      "import/extensions": 0,
+      "@typescript-eslint/no-redeclare": 0,
+      "import/order": 1,
+    },
+    settings: {
+      "import/parsers": {
+        "@typescript-eslint/parser": [".ts", ".tsx"],
+      },
+      "import/resolver": {
+        typescript: {},
+      },
     },
   },
 };
